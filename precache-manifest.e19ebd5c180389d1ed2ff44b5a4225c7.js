7da50bef45880b919b1bb1effa177def5cced9d9
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-04-19 02:41:45 -0400

    Updates

---
+++ precache-manifest.e19ebd5c180389d1ed2ff44b5a4225c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ece5a2554bde65d3e8ecb9de1df3375",
+    "url": "/Corona-React/index.html"
+  },
+  {
+    "revision": "3800be01fc126320ece6",
+    "url": "/Corona-React/static/css/main.7a648731.chunk.css"
+  },
+  {
+    "revision": "dd87d17a3299af60bffc",
+    "url": "/Corona-React/static/js/2.708bb8d5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-React/static/js/2.708bb8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3800be01fc126320ece6",
+    "url": "/Corona-React/static/js/main.ffcb4dfb.chunk.js"
+  },
+  {
+    "revision": "b2edd077f4336c4c9f4e",
+    "url": "/Corona-React/static/js/runtime-main.0086e252.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-React/static/media/image.d7265326.png"
+  }
+]);
